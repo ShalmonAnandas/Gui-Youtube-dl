27a572ecd16f19271b2c8c0aa3a998f9fa657859
Shalmon123 <67845465+Shalmon123@users.noreply.github.com>
2021-01-06 13:37:43 +0530

    Added non youtube video option

--- gui youtube-dl.py
+++ gui youtube-dl.py
@@ -46,6 +46,29 @@ def clip_dl():
     #test
     #print('youtube-dl -v -f 22 '+ link + ' --external-downloader ffmpeg --external-downloader-args "-ss ' + str(startTime) + ' -to '+ str(endTime) + '" -o "' + str(dl_path) + '/%(title)s-%(id)s.%(ext)s" ' + args)
 
+def clip_dl_audio():
+
+    dl_path = askdirectory(title='Select Folder to Save File')
+    #input for links / starttime / endtime
+    link = str(E1.get())
+    startTimeRaw = str(E2.get())
+    endTimeRaw = str(E3.get())
+
+    ##converts for starttime
+    minute2sec1 = startTimeRaw[0] + startTimeRaw[1]
+    sec1 = startTimeRaw[3] + startTimeRaw[4]
+    startTime = int(minute2sec1) * int(60) + int(sec1)
+
+    #conversion for endtime
+    minute2sec2 = endTimeRaw[0] + endTimeRaw[1]
+    sec2 = endTimeRaw[3] + endTimeRaw[4]
+    endTime = int(minute2sec2) * int(60) + int(sec2)
+
+    #run command to download clip
+    os.system('youtube-dl -v -f 140 '+ link + ' --external-downloader ffmpeg --external-downloader-args "-ss ' + str(startTime) + ' -to '+ str(endTime) + '" -o "' + str(dl_path) + '/%(title)s-%(id)s.%(ext)s" ' + args)
+    #test
+    #print('youtube-dl -v -f 22 '+ link + ' --external-downloader ffmpeg --external-downloader-args "-ss ' + str(startTime) + ' -to '+ str(endTime) + '" -o "' + str(dl_path) + '/%(title)s-%(id)s.%(ext)s" ' + args)
+
 
 #code to download mp3
 def mp3_dl():
@@ -72,10 +95,18 @@ def hd_dl():
     #test
     #print('youtube-dl -v -f 22 '+ link + ' -o "' + str(dl_path) + '/%(title)s-%(id)s.%(ext)s" ' + args)
 
+def non_youtube_dl():
+    dl_path = askdirectory(title='Select Folder to Save File')
+    link = str(E1.get())
+    os.system('youtube-dl -v  '+ link + ' -o "' + str(dl_path) + '/%(title)s-%(id)s.%(ext)s" ' + args)
+    #test
+    #print('youtube-dl -v -f 22 '+ link + ' -o "' + str(dl_path) + '/%(title)s-%(id)s.%(ext)s" ' + args)
+
 def video_dl():
     hddl = (var1.get())
     maxdl = (var2.get())
     mp3dl = (var3.get())
+    non = (var4.get())
 
     if hddl == 1:
         hd_dl()
@@ -89,6 +120,11 @@ def video_dl():
 
     if mp3dl == 1:
         mp3_dl()
+    else:
+        pass
+
+    if non == 1:
+        non_youtube_dl()
     else:
         pass
 
@@ -124,13 +160,15 @@ E1.pack(side = LEFT)
 frame.pack(side = TOP)
 
 #checkboxes
-frame = tk.Frame(root, bg="#4c5778",padx = 102, pady = 9)
+frame = tk.Frame(root, bg="#4c5778",padx = 27, pady = 9)
 var1 = IntVar()
 Checkbutton(frame, text="HD(720p)", bg = "#4c5778", activebackground ="#4c5778",  font = "arial 15 bold ", fg="black", activeforeground="black", variable=var1).grid(row=0, column = 0, sticky=W)
 var2 = IntVar()
 Checkbutton(frame, text="Highest Quality", bg = "#4c5778", activebackground ="#4c5778",  font = "arial 15 bold ", fg="black", variable=var2).grid(row=0, column = 1, sticky=W)
 var3 = IntVar()
 Checkbutton(frame, text="Audio(mp3)", bg = "#4c5778", activebackground ="#4c5778",  font = "arial 15 bold ", fg="black", variable=var3).grid(row=0, column = 2, sticky=W)
+var4 = IntVar()
+Checkbutton(frame, text="non-youtube", bg = "#4c5778", activebackground ="#4c5778",  font = "arial 15 bold ", fg="black", variable=var4).grid(row=0, column = 3, sticky=W)
 frame.pack()
 
 
@@ -188,7 +226,11 @@ frame.pack()
 #button for clipdl
 frame = Frame(root, padx = 20, pady=5, bg = "#4c5778")
 B1 = tk.Button(frame, padx=205, pady=2, text="Download Clip", font = "arial 18 bold ", bg="#4c5778", fg="black", command = clip_dl)
-B1.pack()
+B1.pack(side= LEFT)
+"""
+mp3button = tk.Button(frame, padx=64, pady=2, text="Download Clip mp3", font = "arial 14 bold ", bg="#4c5778", fg="black", command = clip_dl_audio)
+mp3button.pack(side = LEFT)
+"""
 frame.pack(side = BOTTOM)
 
 """
